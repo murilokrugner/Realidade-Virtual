d3315bcc12b1621fd9dde0a6f5556bad4e4ca074
murilokrugner <murilokrugner@hotmail.com>
2021-05-11 19:11:39 -0300

    first commit

---
+++ Assets/Resources/Scripts/LoadNextLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public float timeLeft = 5.0f;
+    public TextMeshProUGUI timer; // used for showing countdown from 3, 2, 1 
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime; //timer.text = (timeLeft).ToString("0");
+        timer.SetText(((int)timeLeft).ToString());
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene(1);
+            //Do something useful or Load a new game scene depending on your use-case
+        }
+    }
+
+}
---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //first movement type
+        //
+        //var h = Input.GetAxis("Horizontal");
+        //var v = Input.GetAxis("Vertical");
+
+        //transform.position += new Vector3(h * speed, 0, v * speed);
+        //
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+    }
+
+}
